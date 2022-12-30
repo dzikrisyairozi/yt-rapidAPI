ba9afd98c2d1b7aae4ba0151492b5b1a62f5bfd1
dikiogres <dikiogres@gmail.com>
2022-12-30 16:15:21 +0700

    feat: videos component on feed

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
-import Sidebar from './Sidebar'
+import {Sidebar, Videos} from './'
 
 const Feed = () => {
   return (
@@ -12,6 +12,14 @@ const Feed = () => {
             <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
                 Copyright © 2022 Dzikri Syairozi
             </Typography>
+        </Box>
+
+        <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+            <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+               New <span style={{ color: "#FC1503" }}>Videos</span>
+            </Typography>
+
+            <Videos/>
         </Box>
     </Stack>
   )
--- src/components/index.ts
+++ src/components/index.ts
@@ -7,3 +7,4 @@ export { default as SearchFeed } from './SearchFeed';
 
 export { default as SearchBar } from './SearchBar';
 export { default as Sidebar } from './Sidebar';
+export { default as Videos } from './Videos';
