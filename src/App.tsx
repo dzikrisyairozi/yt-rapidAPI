28a1e00c32436d9c8cf7d919ec8079a0087ccf9e
dikiogres <dikiogres@gmail.com>
2022-12-30 14:36:49 +0700

    feat: modular import layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
 const App = () => (
     <BrowserRouter>
       <Box sx={{ backgroundColor: '#000' }}>
-        Navbar
+        <Navbar/>
         <Routes>
-         <Route exact path='/' element={<Feed />} />
+         <Route path='/' element={<Feed />} />
          <Route path='/video/:id' element={<VideoDetail />} />
          <Route path='/channel/:id' element={<ChannelDetail/>} />
          <Route path='/search/:searchTerm' element={<SearchFeed />} />
